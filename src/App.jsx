0d1200a16184ba10f726923003424a4020ee4979
zaynhakim <zaynhakim@gmail.com>
2022-08-25 15:12:26 +0700

    2 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import CardOne from './components/card'
+import Button from './components/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <div className="flex flex-col space-y-2">
       <CardOne />
+      <Button />
+      </div>
     </div>
   )
 }
---
+++ src/components/button.jsx
@@ -0,0 +1,10 @@
+export default function Button() {
+  return (
+    <button
+      type="button"
+      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 dark:bg-blue-900 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 dark:text-blue-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+    >
+      Whereas recognition of the inherent dignity
+    </button>
+  );
+}
