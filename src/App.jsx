7e7561ce997329c7d06f8bd8191c1b15f73e0b00
zaynhakim <zaynhakim@gmail.com>
2022-09-01 10:02:39 +0700

    6 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
 import CardOne from './components/CardOne';
 import Button from './components/button';
 import CardTwo from './components/CardTwo';
+import NavItems from './components/NavItems';
 
 function App() {
   return (
-    <div className="App">
-      <div className="flex flex-col space-y-2">
+    <div className="App bg-gray-100 dark:bg-gray-900 text-black dark:text-white min-h-screen">
+      <div className="flex flex-col space-y-2 max-w-md mx-auto p-4">
         <CardOne />
         <CardTwo />
         <Button />
+        <NavItems />
       </div>
     </div>
   );
---
+++ src/components/NavItems.jsx
@@ -0,0 +1,78 @@
+export default function NavItems() {
+  return (
+    <div className="mx-auto max-w-md">
+      <nav className="rounded-lg p-3 font-medium">
+        <ul className="flex flex-col space-y-2 w-36">
+          <li className="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100">
+            <button className="flex items-center align rounded-md p-1.5 px-2 w-full">
+              <span className="mr-2.5 inline-block">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                  />
+                </svg>
+              </span>
+              Item 1
+            </button>
+          </li>
+          <li className="text-blue-700">
+            <button className="flex items-center align rounded-md p-1.5 px-2 w-full">
+              <span className="mr-2.5 inline-block">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                  />
+                </svg>
+              </span>
+              Item 2
+            </button>
+          </li>
+          <li className="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100">
+            <button className="flex items-center align rounded-md p-1.5 px-2 w-full">
+              <span className="mr-2.5 inline-block">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 6h.008v.008H6V6z"
+                  />
+                </svg>
+              </span>
+              Item 3
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
